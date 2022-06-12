8f58073dc9f3e1356acfed6f6acaa8000f370bab
RoJuan230 <rohan.nakra100@gmail.com>
2022-06-11 23:37:39 -0700

    V2...

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import time
 import os
+import msvcrt
 
 def start_test():
-    for num in [3, 2, 1]:    # countdown
-        os.system('cls')
-        print(f'{num}...')
-        time.sleep(1)
-        os.system('cls')
+    print('> ')
+    first_letter = msvcrt.getch()
+    os.system('cls')
+    start_time = time.time()    # Once the first letter is recorded, start.
 
-    start_time = time.time()
-    words = input('Go! > ')
+    words = input(f'> {str(first_letter)[2:3]}')
     elapsed = (time.time() - start_time)/60    # divide by 60 to get minutes.
     os.system('cls')
     time.sleep(2)
 
-    words = words.split(' ')
-    print(f'WPM: {len(words)/elapsed:.0f}')
+    adjusted_words_len = (len(words) + 1)/5    # word is defined as every five characters in this case.
+    print(f'WPM: {adjusted_words_len/elapsed:.0f}')
 
     play_again = input('Would you like to play again? ')    
 
@@ -23,15 +22,16 @@ def start_test():
         start_test()
     else:
         return
+    
 
 def welcome():
     os.system('cls')
     print('Welcome to the WPM tester!!')
     time.sleep(2.5)
     os.system('cls')
-    print('Type a phrase or two then hit enter to check out your WPM! You will start in 3 seconds')
+    print('Type a phrase or two then hit enter to check your WPM! Start when you are ready...')
     time.sleep(3.5)
-    
+
     start_test()
 
 welcome()
